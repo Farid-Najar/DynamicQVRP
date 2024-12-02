953c3f824c98366e5c924a5e6b7a3082cd43d577
Farid Najar <47227746+Farid-Najar@users.noreply.github.com>
2024-12-02 20:50:05 +0100

    [Done] new experiments added

--- experiments.py
+++ experiments.py
@@ -41,7 +41,8 @@ def experiment(
             "vehicle_capacity" : 25,
             "re_optimization" : False,
             "costs_KM" : [1, 1],
-            "emissions_KM" : [.1, .3]
+            "emissions_KM" : [.1, .3],
+            "n_scenarios" : 500
         },
     ):
     """Compares different methods implemented so far between them on the 
@@ -53,16 +54,10 @@ def experiment(
         The number of episodes to run for each agent, by default 200
     """
     
+    with open(f'results/env_configs.pkl', 'wb') as f:
+        pickle.dump(env_configs, f)
     
     agents = {
-        "offline" : dict(
-            agentClass = OfflineAgent,
-            env_configs = env_configs,
-            episodes = episodes,
-            agent_configs = {"n_workers": 7},
-            save_results = True,
-            title = "res_offline",
-        ),
         "greedy" : dict(
             agentClass = GreedyAgent,
             env_configs = env_configs,
@@ -78,6 +73,14 @@ def experiment(
             agent_configs = {},
             save_results = True,
             title = "res_random",
+        ),
+        "offline" : dict(
+            agentClass = OfflineAgent,
+            env_configs = env_configs,
+            episodes = episodes,
+            agent_configs = {"n_workers": 7},
+            save_results = True,
+            title = "res_offline",
         ),
         "MSA" : dict(
             agentClass = MSAAgent,
@@ -95,25 +98,28 @@ def experiment(
         
 if __name__ == "__main__":
     # VRP with 2 vehicles
-    # experiment(env_configs = {
-    #         "K" : 50,
-    #         "Q" : 100, 
-    #         "DoD" : 0.5,
-    #         "vehicle_capacity" : 25,
-    #         "re_optimization" : True,
-    #         "costs_KM" : [1, 1],
-    #         "emissions_KM" : [.1, .3]
-    #     },
-    # )
-    
-    # TSP
-    experiment(env_configs = {
+    experiment(
+        500,
+        env_configs = {
             "K" : 50,
-            "Q" : 150, 
-            "DoD" : 0.8,
-            "vehicle_capacity" : 30,
+            "Q" : 70, 
+            "DoD" : 0.7,
+            "vehicle_capacity" : 25,
             "re_optimization" : True,
-            "costs_KM" : [1],
-            "emissions_KM" : [.3]
+            "costs_KM" : [1, 1],
+            "emissions_KM" : [.1, .3],
+            "n_scenarios" : 500
         },
     )
+    
+    # TSP
+    # experiment(env_configs = {
+    #         "K" : 50,
+    #         "Q" : 150, 
+    #         "DoD" : 0.8,
+    #         "vehicle_capacity" : 30,
+    #         "re_optimization" : True,
+    #         "costs_KM" : [1],
+    #         "emissions_KM" : [.3]
+    #     },
+    # )
